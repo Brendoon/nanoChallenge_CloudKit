7f27aaf407ce79fddf11a8d17f0934ae168eb59f
Brendoon <brendoon-ryos@live.com>
2017-10-03 18:31:07 -0300

    Initial commit.

---
+++ NanoChallenge_CloudKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NanoChallenge_CloudKit
+//
+//  Created by Brendoon Ryos on 03/10/17.
+//  Copyright © 2017 Brendoon Ryos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
